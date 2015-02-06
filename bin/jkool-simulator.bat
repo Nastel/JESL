ee39df5ba08904a39eb3e4015cd504058937b7c0
AlbertM <amavashev@nastel.com>
2015-02-06 15:24:40 -0500

    updated tnt4j simulation properties
    updated tnt4j simulation properties

--- bin/jkool-simulator.bat
+++ bin/jkool-simulator.bat
@@ -1,4 +1,4 @@
 @echo off
 set JARDIR=%~p0
 set OPTIONS=-cp "%JARDIR%\..\lib" -Dlog4j.configuration="file:%JARDIR%\..\config\log4j-simulator.properties" -Dtnt4j.config="%JARDIR%\..\config\tnt4j-simulator.properties" -Dtnt4j.token.repository="%JARDIR%\..\tnt4j-tokens.properties"
-java -%OPTIONS% -jar "%JARDIR%\..\jkool-jesl.jar" %*
+java %OPTIONS% -jar "%JARDIR%\..\jkool-jesl.jar" %*

e7293939bb220aed1ba20de002a5d8ad2f95d649
AlbertM <amavashev@nastel.com>
2015-02-09 13:45:52 -0500

    Updated simulator scripts
    Updated simulator scripts

--- bin/jkool-simulator.bat
+++ bin/jkool-simulator.bat
@@ -1,4 +1,14 @@
 @echo off
-set JARDIR=%~p0
-set OPTIONS=-cp "%JARDIR%\..\lib" -Dlog4j.configuration="file:%JARDIR%\..\config\log4j-simulator.properties" -Dtnt4j.config="%JARDIR%\..\config\tnt4j-simulator.properties" -Dtnt4j.token.repository="%JARDIR%\..\tnt4j-tokens.properties"
-java %OPTIONS% -jar "%JARDIR%\..\jkool-jesl.jar" %*
+setlocal
+set SIMDIR=%~p0
+set SIMCMD=run
+if "%1" == "help" (set SIMCMD=help) else set ACCTKN=%1
+set SIMRUNS=1
+if not "%2" == "" set SIMRUNS=%2
+
+set SIMCP=%SIMDIR%..;%SIMDIR%..\jkool-jesl.jar;%SIMDIR%../lib/*
+set SIMMAIN=com.jkool.jesl.simulator.TNT4JSimulator
+set TNT4JOPTS=-Dlog4j.configuration=file:%SIMDIR%..\config\log4j-simulator.properties -Dtnt4j.config=%SIMDIR%..\config\tnt4j-simulator.properties -Dtnt4j.token.repository=%SIMDIR%..\config\tnt4j-tokens.properties
+set SIMFILE=%SIMDIR%\..\sims\order-process.xml
+java %TNT4JOPTS% -classpath %SIMCP% %SIMMAIN% %SIMCMD% -A:%ACCTKN% -f:%SIMFILE% -T:data.jkoolcloud.com -P:6580 -C:HTTP -u -p:98 -i:%SIMRUNS%
+endlocal
--- bin/jkool-simulator.sh
+++ bin/jkool-simulator.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
-origdir=`pwd`
+ORIGDIR=`pwd`
 cd `dirname $0`
-jardir=`pwd`
-cd ${origdir}
-options=-cp ${jardir}/../lib -Dlog4j.configuration=file:${jardir}/../config/log4j-simulator.properties -Dtnt4j.config=${jardir}/../config/tnt4j-simulator.properties -Dtnt4j.token.repository=${jardir}/../tnt4j-tokens.properties
-java ${options} -jar ${jardir}/../jkool-jesl.jar $*
+SIMDIR=`pwd`/
+cd $ORIGDIR
+
+SIMCMD=run
+if [ "$1" == "help" ]; then
+	SIMCMD=help
+else
+	ACCTKN=$1
+fi
+
+SIMRUNS=1
+if [ "$2" != "" ]; then
+	SIMRUNS=$2
+fi
+
+SIMCP=$SIMDIR..:$SIMDIR../jkool-jesl.jar:$SIMDIR../lib/*
+SIMMAIN=com.jkool.jesl.simulator.TNT4JSimulator
+TNT4JOPTS="-Dlog4j.configuration=file:$SIMDIR../config/log4j-simulator.properties -Dtnt4j.config=$SIMDIR../config/tnt4j-simulator.properties -Dtnt4j.token.repository=$SIMDIR../config/tnt4j-tokens.properties"
+SIMFILE=$SIMDIR../sims/order-process.xml
+java $TNT4JOPTS -classpath $SIMCP $SIMMAIN $SIMCMD -A:$ACCTKN -f:$SIMFILE -T:data.jkoolcloud.com -P:6580 -C:HTTP -i:$SIMRUNS -p:98 -u

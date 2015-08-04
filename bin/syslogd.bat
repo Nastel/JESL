96be47d12733a82f710a8e2506c41bce59d6aa18
AlbertM <amavashev@nastel.com>
2015-08-04 11:23:06 -0400

    updated scripts with simple logger config
    updated scripts with simple logger config

--- bin/syslogd.bat
+++ bin/syslogd.bat
@@ -4,6 +4,6 @@ set RUNDIR=%~p0
 
 set CLPATH=%RUNDIR%../jkool-jesl.jar;%RUNDIR%../lib/*
 set MAINCL=com.jkool.jesl.net.syslogd.Syslogd
-set TNT4JOPTS=-Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
+set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
 java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% -h 0.0.0.0 -p 5140 tcp
 endlocal
--- bin/syslogd.sh
+++ bin/syslogd.sh
@@ -3,5 +3,5 @@ RUNDIR=`pwd`
 
 CLPATH="$RUNDIR/../jkool-jesl.jar:$RUNDIR/../lib/*"
 MAINCL=com.jkool.jesl.net.syslogd.Syslogd
-TNT4JOPTS="-Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=$RUNDIR/../config/tnt4j.properties"
+TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=$RUNDIR/../config/tnt4j.properties"
 java $TNT4JOPTS -classpath $CLPATH $MAINCL -h 0.0.0.0 -p 5140 tcp

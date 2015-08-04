a2df6f17482e8f795f3ba97dd1cba0de964641cb
AlbertM <amavashev@nastel.com>
2015-08-04 14:21:34 -0400

    updated scripts, drop log4j dependency
    updated scripts, drop log4j dependency

--- bin/jksim.bat
+++ bin/jksim.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
-set SIMDIR=%~p0
+set RUNDIR=%~p0
 set SIMCMD=run
 if "%1" == "help" (set SIMCMD=help) else set ACCTKN=%1
 set SIMRUNS=1
 if not "%2" == "" set SIMRUNS=%2
 
-set SIMCP=%SIMDIR%..;%SIMDIR%..\jkool-jesl.jar;%SIMDIR%../lib/*
-set SIMMAIN=com.jkool.jesl.simulator.TNT4JSimulator
-set TNT4JOPTS=-Dlog4j.configuration=file:%SIMDIR%..\config\log4j-simulator.properties -Dtnt4j.config=%SIMDIR%..\config\tnt4j-simulator.properties -Dtnt4j.token.repository=%SIMDIR%..\config\tnt4j-tokens.properties
-set SIMFILE=%SIMDIR%\..\sims\order-process.xml
-java %TNT4JOPTS% -classpath %SIMCP% %SIMMAIN% %SIMCMD% -A:%ACCTKN% -f:%SIMFILE% -T:data.jkoolcloud.com -P:6580 -C:HTTP -u -p:98 -i:%SIMRUNS%
+set CLPATH=%RUNDIR%..;%RUNDIR%..\jkool-jesl.jar;%RUNDIR%../lib/*;%RUNDIR%../lib/slf4j-simple/*
+set MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
+set TNT4JOPTS=-Dtnt4j.config=%RUNDIR%..\config\tnt4j-simulator.properties -Dtnt4j.token.repository=%RUNDIR%..\config\tnt4j-tokens.properties
+set SIMFILE=%RUNDIR%\..\sims\order-process.xml
+java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% %SIMCMD% -A:%ACCTKN% -f:%SIMFILE% -T:data.jkoolcloud.com -P:6580 -C:HTTP -u -p:98 -i:%SIMRUNS%
 endlocal
--- bin/jksim.sh
+++ bin/jksim.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ORIGDIR=`pwd`
 cd `dirname $0`
-SIMDIR=`pwd`/
+RUNDIR=`pwd`/
 cd $ORIGDIR
 
 SIMCMD=run
@@ -16,8 +16,8 @@ if [ "$2" != "" ]; then
 	SIMRUNS=$2
 fi
 
-SIMCP="$SIMDIR..:$SIMDIR../jkool-jesl.jar:$SIMDIR../lib/*"
-SIMMAIN=com.jkool.jesl.simulator.TNT4JSimulator
-TNT4JOPTS="-Dlog4j.configuration=file:$SIMDIR../config/log4j-simulator.properties -Dtnt4j.config=$SIMDIR../config/tnt4j-simulator.properties -Dtnt4j.token.repository=$SIMDIR../config/tnt4j-tokens.properties"
-SIMFILE=$SIMDIR../sims/order-process.xml
-java $TNT4JOPTS -classpath $SIMCP $SIMMAIN $SIMCMD -A:$ACCTKN -f:$SIMFILE -T:data.jkoolcloud.com -P:6580 -C:HTTP -i:$SIMRUNS -p:98 -u
+CLPATH="$RUNDIR..:$RUNDIR../jkool-jesl.jar:$RUNDIR../lib/*"
+MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
+TNT4JOPTS="-Dtnt4j.config=$RUNDIR../config/tnt4j-simulator.properties -Dtnt4j.token.repository=$RUNDIR../config/tnt4j-tokens.properties"
+SIMFILE=$RUNDIR../sims/order-process.xml
+java $TNT4JOPTS -classpath $CLPATH $MAINCL $SIMCMD -A:$ACCTKN -f:$SIMFILE -T:data.jkoolcloud.com -P:6580 -C:HTTP -i:$SIMRUNS -p:98 -u

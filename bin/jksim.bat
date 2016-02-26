11851e1a08401c1fba8feb63f233f6b86f462b73
AlbertM <amavashev@nastel.com>
2016-02-26 11:39:30 -0500

    cleanup shell scripts
    cleanup shell scripts

--- bin/jksim.bat
+++ bin/jksim.bat
@@ -1,11 +1,10 @@
 @echo off
 setlocal
 set RUNDIR=%~p0
-set SIMCMD=run
 
 set CLPATH="%RUNDIR%..;%RUNDIR%../jkool-jesl.jar;%RUNDIR%../lib/*;%RUNDIR%../lib/slf4j-simple/*"
 set MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
 set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config="%RUNDIR%../config/tnt4j-simulator.properties" -Dtnt4j.token.repository="%RUNDIR%../config/tnt4j-tokens.properties"
 
-java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% %SIMCMD% -T:data.jkoolcloud.com -P:6585 -C:HTTPS -p:10 -uc -ui %*
+java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% run -T:data.jkoolcloud.com -P:6585 -C:HTTPS -p:10 -uc -ui %*
 endlocal
--- bin/jksim.sh
+++ bin/jksim.sh
@@ -8,4 +8,4 @@ CLPATH="$RUNDIR..:$RUNDIR../jkool-jesl.jar:$RUNDIR../lib/*"
 MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
 TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config="$RUNDIR../config/tnt4j-simulator.properties" -Dtnt4j.token.repository="$RUNDIR../config/tnt4j-tokens.properties"
 
-java $TNT4JOPTS -classpath $CLPATH $MAINCL $SIMCMD -T:data.jkoolcloud.com -P:6585 -C:HTTPS -p:10 -uc -ui $@
+java $TNT4JOPTS -classpath $CLPATH $MAINCL run -T:data.jkoolcloud.com -P:6585 -C:HTTPS -p:10 -uc -ui $@

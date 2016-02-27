4af4655186592586c6d208a1e4827f0973737da2
AlbertM <amavashev@nastel.com>
2016-02-27 14:54:37 -0500

    updated classpath
    updated classpath

--- bin/jksim.sh
+++ bin/jksim.sh
@@ -5,7 +5,7 @@ RUNDIR=`pwd`
 cd $ORIGDIR
 
 
-CLPATH="$RUNDIR/..:$RUNDIR/../jkool-jesl.jar:$RUNDIR/../lib/*"
+CLPATH="$RUNDIR/..:$RUNDIR/../jkool-jesl.jar:$RUNDIR/../lib/*:$RUNDIR/../lib/slf4j-simple/*"
 MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
 TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config=$RUNDIR/../config/tnt4j-simulator.properties"
 

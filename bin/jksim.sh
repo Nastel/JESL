71fbcda3d95796fc2c818b53d2a94715f8ca1b7d
AlbertM <amavashev@nastel.com>
2016-02-27 09:06:27 -0500

    convert UNIX scripts to unix new line
    convert UNIX scripts to unix new line

--- bin/jksim.sh
+++ bin/jksim.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 ORIGDIR=`pwd`
 cd `dirname $0`
-RUNDIR=`pwd`/
+RUNDIR=`pwd`
 cd $ORIGDIR
 
-CLPATH="$RUNDIR..:$RUNDIR../jkool-jesl.jar:$RUNDIR../lib/*"
+
+CLPATH="$RUNDIR/..:$RUNDIR/../jkool-jesl.jar:$RUNDIR/../lib/*"
 MAINCL=com.jkool.jesl.simulator.TNT4JSimulator
-TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config="$RUNDIR../config/tnt4j-simulator.properties" -Dtnt4j.token.repository="$RUNDIR../config/tnt4j-tokens.properties"
+TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config=$RUNDIR/../config/tnt4j-simulator.properties"
 
 java $TNT4JOPTS -classpath $CLPATH $MAINCL run -T:data.jkoolcloud.com -P:6585 -C:HTTPS -p:10 -uc -ui $@

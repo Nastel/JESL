ebc922a38c0710633b0e04fd7407143ab00cf190
AlbertM <amavashev@nastel.com>
2016-07-15 13:42:34 -0400

    update debug levels
    update debug levels

--- bin/jksysd.bat
+++ bin/jksysd.bat
@@ -4,6 +4,6 @@ set RUNDIR=%~p0
 
 set CLPATH="%RUNDIR%../*.jar;%RUNDIR%../lib/*"
 set MAINCL=com.jkoolcloud.jesl.net.syslogd.Syslogd
-set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
+set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
 java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% -h 0.0.0.0 -p 5140 tcp
 endlocal

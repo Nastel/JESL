4bb4bef7a31ceef0482c49117fbb8635df38a953
AlbertM <amavashev@nastel.com>
2018-10-30 12:24:01 -0400

    Update shell scripts
    Update shell scripts

--- bin/jksysd.sh
+++ bin/jksysd.sh
@@ -3,6 +3,6 @@ RUNDIR=`pwd`
 
 CLPATH="$RUNDIR/../*.jar:$RUNDIR/../lib/*"
 MAINCL=com.jkoolcloud.jesl.net.syslogd.Syslogd
-TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=$RUNDIR/../config/tnt4j.properties"
+TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=$RUNDIR/../config/tnt4j.properties -Dtnt4j.config.path=$RUNDIR/../config/"
 
 java $TNT4JOPTS -classpath $CLPATH $MAINCL -h 0.0.0.0 -p 5140 tcp

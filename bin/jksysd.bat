d8fa3848b5ab936b887b04f9c3d7af0fafd50392
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2018-11-26 18:17:48 +0200

    cleanup, common configuration root path now can be resolved from `tnt4j.config` property defined file.

--- bin/jksysd.bat
+++ bin/jksysd.bat
@@ -4,6 +4,6 @@ set RUNDIR=%~dp0
 
 set CLPATH="%RUNDIR%../*;%RUNDIR%../lib/*"
 set MAINCL=com.jkoolcloud.jesl.net.syslogd.Syslogd
-set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties -Dtnt4j.config.path=%RUNDIR%../config/
+set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
 java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% -h 0.0.0.0 -p 5140 tcp
 endlocal

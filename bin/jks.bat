7b9cfea077f97fba6a7e2a248f52450c97863c32
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2022-03-17 09:43:01 +0200

    Changed path delimiters from Unix to Windows.

--- bin/jks.bat
+++ bin/jks.bat
@@ -2,9 +2,9 @@
 setlocal
 set RUNDIR=%~dp0
 
-set CLPATH="%RUNDIR%..;%RUNDIR%../*;%RUNDIR%../lib/*"
+set CLPATH="%RUNDIR%..;%RUNDIR%..\*;%RUNDIR%..\lib\*"
 set MAINCL=com.jkoolcloud.jesl.simulator.TNT4JSimulator
-set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config="%RUNDIR%../config/tnt4j-simulator.properties" -Dtnt4j.token.repository="%RUNDIR%../config/tnt4j-tokens.properties"
+set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config="%RUNDIR%..\config\tnt4j-simulator.properties" -Dtnt4j.token.repository="%RUNDIR%..\config\tnt4j-tokens.properties"
 
 java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% %*
 endlocal

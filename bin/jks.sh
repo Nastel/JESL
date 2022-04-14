0823289478439b45e2903f0c17cbcaaabdbb6f9f
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2022-04-14 15:26:44 +0300

    Improved RUNDIR resolution.

--- bin/jks.sh
+++ bin/jks.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-ORIGDIR=`pwd`
-cd `dirname $0`
-RUNDIR=`pwd`
-cd $ORIGDIR
+if command -v readlink >/dev/null 2>&1; then
+    RUNDIR=$(dirname $(readlink -m $BASH_SOURCE))
+else
+    RUNDIR=$(cd "$(dirname "$BASH_SOURCE")" ; pwd -P)
+fi
 
-
+TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config=$RUNDIR/../config/tnt4j-simulator.properties"
 CLPATH="$RUNDIR/..:$RUNDIR/../*:$RUNDIR/../lib/*"
 MAINCL=com.jkoolcloud.jesl.simulator.TNT4JSimulator
-TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.config=$RUNDIR/../config/tnt4j-simulator.properties"
 
-java $TNT4JOPTS -classpath $CLPATH $MAINCL $@
+java $TNT4JOPTS -classpath "$CLPATH" $MAINCL $@
